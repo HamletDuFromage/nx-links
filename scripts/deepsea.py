1e481cf31300f220e514019469dbe5d6e3f1c1f8
flb <f.lb@gmx.com>
2021-06-22 14:22:31 +0200

    update scripts

---
+++ scripts/deepsea.py
@@ -0,0 +1,23 @@
+from basemodule import Basemodule
+from pathlib import Path
+import configparser
+import os
+import json
+parser = configparser.ConfigParser()
+
+config = [
+    {
+        "service": 1,
+        "username": "Team-Neptune",
+        "reponame": "DeepSea",
+        "assetRegex": "",
+        "assetPatterns": [".*deepsea.*\\.zip"]
+    }
+]
+
+class Deepsea(Basemodule):
+    def __init__(self, config):
+        self.path = "ams.json"
+        Basemodule.__init__(self, config)
+
+package = Deepsea(config)
--- scripts/main.py
+++ scripts/main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     requiredNamed.add_argument('-gt', '--githubToken', help='Github Token', required=True)
     args = parser.parse_args()
     importlib.import_module("atmosphere")
+    importlib.import_module("deepsea")
     importlib.import_module("cfw")
     importlib.import_module("sigpatches")
     importlib.import_module("firmwares")
