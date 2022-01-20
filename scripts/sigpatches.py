6a47a370390cb1b658675f65d6cf084ba4efd664
flb <f.lb@gmx.com>
2022-01-20 15:00:35 +0100

    update sigpatches asset name

--- scripts/sigpatches.py
+++ scripts/sigpatches.py
@@ -13,3 +13,10 @@ class Sigpatches(BaseModule):
             }
         ]
         BaseModule.__init__(self)
+
+    def handle_module(self):
+        for i in range(len(self.config)):
+            release = self.get_latest_release(i)
+            assets = self.get_asset_links(release, i)
+            for asset in assets:
+                self.out[release.title] = asset.browser_download_url
