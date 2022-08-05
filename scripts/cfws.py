853c183146f8ac68cd31060ae1fd3ac601b00013
PoloNX <geyrecharlie@gmail.com>
2022-08-05 12:31:05 +0200

    fix

--- scripts/cfws.py
+++ scripts/cfws.py
@@ -12,7 +12,9 @@ class Cfws(BaseModule):
         BaseModule.__init__(self)
 
     def handle_module(self):
-        release = self.get_latest_release(0)
-        assets = self.get_asset_links(release, self.config[0]["assetPatterns"][0])
-        self.out[release.title] = asset[0].browser_download_url
-        self.out["version"] = release.tag_name
+        for i in range(len(self.config)):
+            release = self.get_latest_release(i)
+            assets = self.get_asset_links(release, i)
+            self.out["version"] = release.tag_name
+            for asset in assets:
+                self.out[release.title] = asset.browser_download_url
