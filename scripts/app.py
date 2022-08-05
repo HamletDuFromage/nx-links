1d76bbf30451650a2a1a3f1b37d8e1f2a3cdc402
PoloNX <geyrecharlie@gmail.com>
2022-08-05 12:25:30 +0200

    fix asset[0]

--- scripts/app.py
+++ scripts/app.py
@@ -14,5 +14,5 @@ class App(BaseModule):
     def handle_module(self):
         release = self.get_latest_release(0)
         asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
-        self.out["AtmoPackUpdater"] = asset.browser_download_url
+        self.out["AtmoPackUpdater"] = asset[0].browser_download_url
         self.out["version"] = release.tag_name
