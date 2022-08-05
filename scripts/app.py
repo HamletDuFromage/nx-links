5a748e54aa048537fb8cea4fe24f07cabbc5d64e
PoloNX <geyrecharlie@gmail.com>
2022-08-05 12:22:21 +0200

    fix by (@pharuxtan)

--- scripts/app.py
+++ scripts/app.py
@@ -12,8 +12,7 @@ class App(BaseModule):
         BaseModule.__init__(self)
 
     def handle_module(self):
-        for i in range(len(self.config)):
-            release = self.get_latest_release(i)
-            assets = self.get_asset_links(release, i)
-            for asset in assets:
-                self.out[release.title] = asset.browser_download_url
+        release = self.get_latest_release(0)
+        asset = self.get_asset_link(release, self.config[0]["assetPatterns"][0])
+        self.out["AtmoPackUpdater"] = asset.browser_download_url
+        self.out["version"] = release.tag_name
